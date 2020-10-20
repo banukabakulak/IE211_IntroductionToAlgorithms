ce7a4f45a29e2f394add480384ba831d88015ddb
Banu <banu.kabakulak@bilgi.edu.tr>
2020-10-20 14:47:12 +0300

    Lab 2 codes added...

---
+++ IE 211 Lab/src/Lab2/Lab2.java
@@ -0,0 +1,76 @@
+package Lab2;
+
+import java.util.Random;
+
+public class Lab2 {
+	
+	final static int SIZE = 20;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random rand = new Random();
+		
+		// SUBSET GENERATION ALGORITHM 
+		
+		// STEP 1: Create input instance randomly 
+		
+		int[] set = new int[SIZE];
+			
+		for(int i = 0; i < set.length; i++) 
+		{			
+			set[i] = 2 *i + 1;
+			System.out.print(set[i] + " ");
+		}
+		
+		// STEP 2: Implement Subset Generation by calling the function of Subset class
+		
+		Subset mySubsets = new Subset();
+		
+		mySubsets.GenerateSubsets(set);
+		
+		
+		
+//		// STEP 1: Create input instance randomly
+//		
+//		System.out.println("The initial sequence is : ");
+//		
+//		int[] nValues = new int[SIZE];
+//		
+//		for(int i = 0; i < nValues.length; i++)
+//		{
+//			// randomly generate values between [0, 50)
+//			nValues[i] = rand.nextInt(50);
+//			
+//			System.out.print(nValues[i] + " ");
+//		}
+//		
+//		// STEP 2: Implement Insertion Sort by calling the function of the Sort class
+//		
+//		Sort insertionSort = new Sort();
+//		insertionSort.InsertionSort(nValues);	
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ IE 211 Lab/src/Lab2/Subset.java
@@ -0,0 +1,74 @@
+package Lab2;
+
+import java.util.BitSet;
+import java.util.Vector;
+import java.time.Clock; 
+
+public class Subset {
+	
+	private int setSize;
+	
+	private double runTime;
+	
+	private Vector binVal;
+	
+	public Subset() 
+	{
+		binVal = new Vector();
+	}
+	
+	private void Binary(int n)
+	{
+		if(n > 1)
+			Binary(n/2);
+		
+		binVal.add(0, n % 2);
+	}
+	
+	public void GenerateSubsets(int[] set)
+	{
+		System.out.println();
+		
+//		// test the Binary() function 
+//		Binary(12);
+//		
+//		for(int i = 0; i< binVal.size(); i++)
+//			System.out.print(binVal.get(i) + " ");
+//		
+//		// end of test
+		
+		
+		setSize = set.length;
+		
+		System.out.println();
+		
+		Clock clock = Clock.systemDefaultZone();
+		runTime  = clock.millis();
+		
+		for(int j = 0; j < Math.pow(2,setSize); j++ )
+		{
+			binVal.clear();
+			
+			Binary(j);
+			
+//			for(int i = 0; i< binVal.size(); i++)
+//				System.out.print(binVal.get(i) + " ");
+			
+			System.out.println("The " + j + "th subset is: ");
+			
+			for(int i = 0; i< binVal.size(); i++)
+				if((int)binVal.get(i) > 0)
+					System.out.print(set[i] + " ");
+			
+			System.out.println();
+		}
+		
+		runTime = clock.millis() - runTime;
+		
+		System.out.println();
+		
+		System.out.println("The runtime: " + runTime);		
+		
+	}
+
+}
