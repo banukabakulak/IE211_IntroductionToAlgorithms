6071987b4201816adfa3ce638ee3bb1a49bf977d
Banu <banu.kabakulak@bilgi.edu.tr>
2020-11-03 15:51:48 +0300

    Lab 4 Codes

---
+++ IE 211 Lab/src/Lab3/Lab3.java
@@ -0,0 +1,31 @@
+package Lab3;
+
+public class Lab3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+
+		int[] array1 = {13, 21, 36, 48, 42, 15};
+		
+		int[] array2 = {10, 12, 13, 14, 15, 16};
+		
+		System.out.println();
+		System.out.println("the first array is ");
+		for(int i = 0; i < array1.length; i++)
+			System.out.print(array1[i] + " ");
+		
+		System.out.println();
+		System.out.println("the second array is ");
+		for(int i = 0; i < array2.length; i++)
+			System.out.print(array2[i] + " ");
+		
+		System.out.println();
+		
+//		Difference myDiff = new Difference();
+//		myDiff.printDifferentElements(array1, array2);
+
+	}
+
+}
