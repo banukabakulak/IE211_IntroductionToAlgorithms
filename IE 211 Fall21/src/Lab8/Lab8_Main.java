f0f0aefa4af8d381fda6cdb848db4859b465d77d
Banu <banu.kabakulak@bilgi.edu.tr>
2022-01-14 07:38:12 +0300

    Lab8 codes...

---
+++ IE 211 Fall21/src/Lab8/Lab8_Main.java
@@ -0,0 +1,13 @@
+package Lab8;
+
+public class Lab8_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 DataStructures myData = new DataStructures();
+		 myData.ImplementAlgorithm();
+		 
+	}
+
+}
